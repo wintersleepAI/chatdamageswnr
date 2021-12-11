3cc1efeac168a44cda4c503cbd0fe6dbf863a935
WintersleepAI <wintersleep@protonmail.com>
2021-12-10 23:26:45 -0600

    fixes to account for modifiers and shock dmg

--- chatdamageswnr.js
+++ chatdamageswnr.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 // This code was modelled on chatdamagebuttons5e.js
 
 class ChatDamageButtonsSWNR extends Application {
@@ -8,18 +9,22 @@ class ChatDamageButtonsSWNR extends Application {
     init () {
 
         Hooks.on('renderChatMessage', (message, html, data) => {            
-            if ( !message.isRoll) return
+            if ( !message.isRoll) return;
             
-            let len = message.roll.dice.length;      
+            let len = message.roll.terms[0].results.length;
             // If there are 2 rolls, use the second. else take first      
             let total = 0;
+            let shock = null;
             if (len == 0) {
                 // Shock Damage with no roll
                 total = message.roll.total;
             } else if (len == 1) {
-                total = message.roll.dice[0].total;
+                total = message.roll.terms[0].results[0].result;
             } else if (len == 2) {
-                total = message.roll.dice[1].total;
+                total = message.roll.terms[0].results[1].result;
+            } else if (len == 3) {
+                total = message.roll.terms[0].results[1].result;
+                shock = message.roll.terms[0].results[2].result;
             } else {
                 console.log("Unknown roll. Not trying to set damage button", message);
                 return;
@@ -28,10 +33,10 @@ class ChatDamageButtonsSWNR extends Application {
 
             let btnStyling = 'width: 22px; height:22px; font-size:10px;line-height:1px';
 
-            const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply full damage to selected token(s)."></i></button>`);
+            const fullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply full damage (${total}) to selected token(s)."></i></button>`);
             // const halfDamageButton = $(`<button class="dice-total-halfDamage-btn" style="${btnStyling}"><i class="fas fa-user-shield" title="Click to apply half damage to selected token(s)."></i></button>`);
             // const doubleDamageButton = $(`<button class="dice-total-doubleDamage-btn" style="${btnStyling}"><i class="fas fa-user-injured" title="Click to apply double damage to selected token(s)."></i></button>`);
-            const fullHealingButton = $(`<button class="dice-total-fullHealing-btn" style="${btnStyling}"><i class="fas fa-user-plus" title="Click to apply full healing to selected token(s)."></i></button>`);
+            const fullHealingButton = $(`<button class="dice-total-fullHealing-btn" style="${btnStyling}"><i class="fas fa-user-plus" title="Click to apply full healing (${total}) to selected token(s)."></i></button>`);
 
             const btnContainer = $('<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>');
             btnContainer.append(fullDamageButton);
@@ -41,7 +46,12 @@ class ChatDamageButtonsSWNR extends Application {
             //console.log()
 
             //Find the first or second div to append the button to
-            html.find('.dice-total').slice(len-1).append(btnContainer);
+            if (shock == null){
+                html.find('.dice-total').slice(len-1, len).append(btnContainer);
+            } else {
+                // shock is last, need to put at 2nd to last
+                html.find('.dice-total').slice(len-2, len-1).append(btnContainer);
+            }
 
             // Handle button clicks
             fullDamageButton.click(ev => {
@@ -64,6 +74,27 @@ class ChatDamageButtonsSWNR extends Application {
                 ev.stopPropagation();
                 applyHealthDrop(total*-1);
             });
+
+            if (shock) {
+                const shockFullDamageButton = $(`<button class="dice-total-fullDamage-btn" style="${btnStyling}"><i class="fas fa-user-minus" title="Click to apply full shock damage (${shock}) to selected token(s)."></i></button>`);
+                const shockFullHealingButton = $(`<button class="dice-total-fullHealing-btn" style="${btnStyling}"><i class="fas fa-user-plus" title="Click to apply full shock healing (${shock}) to selected token(s)."></i></button>`);
+
+                const shockbtnContainer = $('<span class="dmgBtn-container" style="position:absolute; right:0; bottom:1px;"></span>');
+                shockbtnContainer.append(shockFullDamageButton);
+                shockbtnContainer.append(shockFullHealingButton);
+                html.find('.dice-total').slice(len-1).append(shockbtnContainer);
+
+                // Handle button clicks
+                shockFullDamageButton.click(ev => {
+                    ev.stopPropagation();
+                    applyHealthDrop(shock);
+                });
+
+                shockFullHealingButton.click(ev => {
+                    ev.stopPropagation();
+                    applyHealthDrop(shock*-1);
+                });
+            }
         
         
         });
@@ -71,12 +102,12 @@ class ChatDamageButtonsSWNR extends Application {
 }
 
 async function applyHealthDrop(total) {
-    let actors = canvas.tokens.controlled.map(token => {return token.actor});
+    let actors = canvas.tokens.controlled.map(token => {return token.actor;});
     if(actors.length == 0){
-      ui.notifications.error("Please select at least one token")
+      ui.notifications.error("Please select at least one token");
       return;
     }
-    console.log(`Applying health drop ${total} to ${actors.length} selected tokens`);
+    //console.log(`Applying health drop ${total} to ${actors.length} selected tokens`);
 
     for (let actor of actors){
         let newHealth = actor.data.data.health.value - total;
@@ -85,8 +116,8 @@ async function applyHealthDrop(total) {
         } else if (newHealth > actor.data.data.health.max)  {
             newHealth = actor.data.data.health.max;
         }
-        console.log(`Updating ${actor.name} health to ${newHealth}`);
-        actor.update({"data.health.value": newHealth});
+        //console.log(`Updating ${actor.name} health to ${newHealth}`);
+        await actor.update({"data.health.value": newHealth});
     }
 }
 
